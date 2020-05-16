4a1ad0c1e5072d671fff57c53e393e16b54e91b1
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-05-16 01:32:29 -0500

    Agregar archivos de proyecto.

---
+++ MasterDetail/MasterDetail/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterDetail
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new MasterDetailPage1Detail();
+            MainPage = new MasterDetailPage1();
+            //MainPage = new MasterDetailPage1Master();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
